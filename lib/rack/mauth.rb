2e630f16823b8b444c3b7b2e60bd27c09f4284fc
Aaron Suggs <aaron@ktheory.com>
2011-06-30 17:36:29 -0400

    Allow local authentication

--- lib/rack/mauth.rb
+++ lib/rack/mauth.rb
@@ -2,11 +2,19 @@ require 'json'
 require 'base64'
 require 'uri'
 require 'net/https'
+require 'thread'
+require 'mauth_signer'
 
 module Medidata
-  class MAuth
-    def initialize(app, mauth_endpoint)
-      @app, @mauth_endpoint = app, URI.parse(mauth_endpoint)
+  class MAuthMiddleware
+    class MissingCacheKey   < Exception; end
+    class VerficationFailed < Exception; end
+
+    def initialize(app, config)
+      @app, @mauth_baseurl, @app_uuid, @private_key = app, config[:mauth_baseurl], config[:app_uuid], config[:private_key]
+      @cached_secrets_mutex = Mutex.new
+      @cached_secrets = {}
+      @mauth_signer = MAuth::Signer.new(@private_key) if can_authenticate_locally?
     end
 
     def call(env)
@@ -17,39 +25,124 @@ module Medidata
       end
     end
 
-    # Determine if the given endpoint should be authenticated. Perhaps use env['PATH_INFO']
-    def should_authenticate?(env)
-      true
-    end
-
-    def authenticated?(env)
-      mws_token, auth_info =  env['HTTP_AUTHORIZATION'].to_s.split(' ')
-      app_uuid, digest = auth_info.split(':') if auth_info
-
-      return false unless app_uuid && digest && mws_token == 'MWS'
-
-      data = {
-        'verb' => env['REQUEST_METHOD'],
-        'app_uuid' => app_uuid,
-        'client_signature' => digest,
-        'request_url' => env['HTTP_HOST'] + env['REQUEST_URI'],
-        'request_time' => env['HTTP_X_MWS_TIME']
-      }
-      if env['REQUEST_METHOD'] == 'POST'
-        data['b64encoded_post_data'] = Base64.encode64(env['rack.input'].read)
-        env['rack.input'].rewind
+    protected
+      def authentication_url
+        URI.parse(@mauth_baseurl + "/authentication_tickets.json")
       end
 
+      def security_tokens_path
+        "/security_tokens.json"
+      end
 
-      # Post to endpoint
-      response = Net::HTTP.post_form(@mauth_endpoint, 'data' => data.to_json)
+      def security_tokens_url
+        URI.parse(@mauth_baseurl + security_tokens_path)
+      end
 
-      response.code.to_i == 204
+      def synchronize
+        @cached_secrets_mutex.synchronize { yield }
+      end
+
+      def cache_expired?
+        last_refresh = synchronize { @last_refresh }
+        puts last_refresh.inspect
+        last_refresh.nil? || last_refresh < (Time.now - 30)
+      end
+
+      def secret_for_app(app_uuid)
+        secret = synchronize { @cached_secrets[app_uuid] }
+        raise MissingCacheKey if secret.nil?
+        secret
+      end
+
+      def refresh_cache
+        synchronize { @last_refresh = Time.now }
+        headers = @mauth_signer.signed_headers(@app_uuid, 'GET', security_tokens_path)
+
+        response = Net::HTTP.start(security_tokens_url.host, security_tokens_url.port) {|http|
+          http.get(security_tokens_url.path, headers)
+        }
+        synchronize do
+
+          @cached_secrets = JSON.parse(response.body).inject({}){|h, token|
+            key = token['security_token']['app_uuid']
+            val = token['security_token']['private_key']
+            h[key] = val
+            h
+          }
+        end
+      end
+
+      # Determine if the given endpoint should be authenticated. Perhaps use env['PATH_INFO']
+      def should_authenticate?(env)
+        # Something like
+        #env['PATH_INFO'] =~ /^\/api/
+        true
+      end
+
+      def can_authenticate_locally?
+        @app_uuid && @private_key
+      end
+
+      def authenticated?(env)
+        mws_token, auth_info =  env['HTTP_AUTHORIZATION'].to_s.split(' ')
+        app_uuid, digest = auth_info.split(':') if auth_info
+
+        return false unless app_uuid && digest && mws_token == 'MWS'
+
+        verb = env['REQUEST_METHOD']
+        request_url = env['REQUEST_URI']
+        time = env['HTTP_X_MWS_TIME']
+        post_data = ''
+        if %w(POST PUT).include? env['REQUEST_METHOD']
+          post_data = Base64.encode64(env['rack.input'].read)
+          env['rack.input'].rewind
+        end
+
+        if can_authenticate_locally?
+          authenticate_locally(digest, app_uuid, verb, request_url, time, post_data)
+       else
+          authenticate_remotely(digest, app_uuid, verb, request_url, time, post_data)
+        end
+      end
+      def authenticate_locally(digest, app_uuid, verb, request_url, time, post_data)
+        begin
+          secret = secret_for_app(app_uuid)
+          if MAuth::Signer.new(secret).verify(digest, app_uuid, verb, request_url, time, post_data)
+           return true
+          else
+            raise VerficationFailed
+          end
+
+        rescue MissingCacheKey, VerficationFailed
+          if cache_expired?
+            refresh_cache
+            retry
+          else
+            return false
+          end
+        end
+      end
+
+      def authenticate_remotely(digest, app_uuid, verb, request_url, time, post_data)
+
+        data = {
+          'verb' => verb,
+          'app_uuid' => app_uuid,
+          'client_signature' => digest,
+          'request_url' => request_url,
+          'request_time' => time,
+          'b64encoded_post_data' => Base64.encode64(post_data)
+        }
+
+        # Post to endpoint
+        response = Net::HTTP.post_form(authentication_url, 'data' => data.to_json)
+
+        response.code.to_i == 204
+      end
+
+      def unauthenticated_response
+        [401, {'Content-Type' => 'text/plain'}, 'Unauthorized']
+      end
+
     end
-
-    def unauthenticated_response
-      [401, {'Content-Type' => 'text/plain'}, 'Unauthorized']
-    end
-
-  end
 end
--- test.ru
+++ test.ru
@@ -1,11 +1,26 @@
 require 'rubygems'
 require 'rack'
 
+private_key = "secret"
 
 simple_app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, "Hello Rack\n"] }
 
+config = {
+  :mauth_baseurl => 'http://localhost:3001',
+
+  # NB: private_key and app_uuid enable local authentication.
+  # They'll only work if the app_uuid can request private keys in MAuth.
+  # Authentication won't work if they're provided and the app doesn't have permission in MAuth to request private keys
+  :private_key => private_key,
+  :app_uuid => '1234'
+}
+
 $: << 'lib'
 require 'rack/mauth'
-use Medidata::MAuth, 'http://localhost:3001/authentication_tickets.json'
+
+require 'ruby-debug'
+
+#config = { :mauth_baseurl => 'http://localhost:3001'}
+use Medidata::MAuthMiddleware, config
 run simple_app
 
