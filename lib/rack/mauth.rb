e851d3a8791f09bcc1338499db0a06ab4527c7d5
Matthew Szenher <mszenher@mdsol.com>
2012-01-11 17:47:28 -0500

    cache mauth signers instead of public keys; reorg.

--- lib/rack/mauth.rb
+++ lib/rack/mauth.rb
@@ -11,15 +11,15 @@ module Medidata
 
     # Middleware initializer
     def initialize(app, config = {})
-      @mauth_baseurl = config[:mauth_baseurl] || raise ArgumentError, 'missing base url'
-      @version = config[:version] || raise ArgumentError, 'missing api version'
+      @mauth_baseurl = config[:mauth_baseurl] || (raise ArgumentError, 'missing base url')
+      @version = config[:version] || (raise ArgumentError, 'missing api version')
       verify_mauth_baseurl
       
-      @app, @app_uuid, @private_key = app, config[:app_uuid], config[:private_key]
+      @app, @self_app_uuid, @private_key = app, config[:app_uuid], config[:private_key]
       @path_whitelist, @whitelist_exceptions = config[:path_whitelist], config[:whitelist_exceptions]
-      @cached_secrets_mutex = Mutex.new
-      @cached_secrets = {}
-      @mauth_signer = MAuth::Signer.new(@private_key) if can_authenticate_locally?      
+      @cached_signers_mutex = Mutex.new
+      @cached_signers = {}
+      @mauth_signer_for_self = MAuth::Signer.new(:private_key => @private_key) if can_authenticate_locally?      
     end
 
     # Method called by app using middleware
@@ -55,52 +55,7 @@ module Medidata
 
       # Synchronize ivars
       def synchronize
-        @cached_secrets_mutex.synchronize { yield }
-      end
-
-      # Cache for a given token expires every minute
-      def token_expired?(token)
-        token.nil? || token[:last_refresh] < (Time.now - 60)
-      end
-
-      # Find the cached secret for app with given app_uuid
-      def public_key_for_app(app_uuid)
-        tok = fetch_cached_token(app_uuid)
-        refresh_token(app_uuid) if token_expired?(sec)
-        key = fetch_public_key(app_uuid)
-
-        log("Cannot find secret for app with uuid #{app_uuid}") unless key
-        key
-      end
-
-      def fetch_cached_token(app_uuid)
-        synchronize { @cached_secrets[app_uuid] }
-      end
-
-      def fetch_public_key(app_uuid)
-        sec = fetch_cached_token(app_uuid)
-        synchronize { sec.nil? ? nil : sec[:private_key]}
-      end
-
-      # Refresh information for a token from mAuth
-      def refresh_token(app_uuid)
-        remote_secret = get_remote_secret(app_uuid)
-        remote_key_pair = parse_secret(remote_secret) if remote_secret
-        synch_cache(remote_key_pair, app_uuid)
-        remote_key_pair
-      end
-
-      def synch_cache(remote_key_pair, app_uuid)
-        synchronize do
-          if remote_key_pair.nil?
-            @cached_secrets.delete(app_uuid)
-          else
-            remote_app_uuid = remote_key_pair[app_uuid]
-            @cached_secrets[app_uuid] = {}
-            @cached_secrets[app_uuid][:private_key] = remote_app_uuid[:private_key]
-            @cached_secrets[app_uuid][:last_refresh] = Time.now
-          end
-        end
+        @cached_signers_mutex.synchronize { yield }
       end
 
       # Parse secret from mAuth
@@ -112,26 +67,6 @@ module Medidata
           return {key => {:private_key => val}}
         rescue JSON::ParserError, TypeError
           log "Cannot parse JSON response for shared secret request from mAuth:  #{secret_from_mauth}"
-        end
-      end
-
-      def get_remote_secret(app_uuid)
-        headers = @mauth_signer.signed_headers(:app_uuid => @app_uuid, :verb => 'GET', :request_url => security_token_path(app_uuid))
-        response = get(security_token_url(app_uuid), {:headers => headers})
-
-        return according_to(response, app_uuid)
-      end
-
-      # Returns nil when a token should be removed or anything else to add to the cache
-      def according_to(response, app_uuid)
-        return mauth_server_error(app_uuid) if response.nil?
-        case response.code.to_i
-        when 200 then return response.body
-        when 404 then return nil
-        when 500 then return mauth_server_error(app_uuid)
-        else
-          log "Attempt to refresh cache with secret from mAuth responded with #{response.code.to_i} #{response.body} for #{app_uuid}"
-          return nil
         end
       end
 
@@ -158,7 +93,7 @@ module Medidata
 
       # Rack-mauth can authenticate locally if the middleware user provided an app_uuid and private_key
       def can_authenticate_locally?
-        @app_uuid && @private_key
+        @self_app_uuid && @private_key
       end
 
       # Is the request authentic?
@@ -167,7 +102,6 @@ module Medidata
         app_uuid, digest = auth_info.split(':') if auth_info
 
         return false unless app_uuid && digest && mws_token == 'MWS'
-
 
         #TODO Find a rack env like 'PATH_INFO' that all servers will accept as the relative URI
         # ie rack and rails have different REQUEST_URI
@@ -191,10 +125,61 @@ module Medidata
         end
       end
 
+      # Add, replace or delete signer from cache
+      # If add/replace, then update refreshness time
+      def update_cache(app_uuid, new_signer, action)
+        synchronize do
+          if action == :delete
+            @cached_signers.delete(app_uuid)
+          elsif action == :add
+            @cached_signers[app_uuid] = {:signer => new_signer, :last_refresh => Time.now} # cached signers are actually hashes of the form {:signer => the_signer, :last_refresh => the time of last refresh}
+          end
+        end
+      end
+      
+      # Make a new signer with info. (i.e. public key) from MAuth
+      # if MAuth returns 200, then add to/replace in cache
+      # if MAuth returns 404, then remove from cache
+      # if MAuth return 500, do nothing
+      def refresh_signer(app_uuid)
+        ret = get_remote_public_key(app_uuid)
+        
+        if ret[:status_code] == 200
+          update_cache(app_uuid, new_signer = MAuth::Signer.new(:public_key => ret[:public_key]), :add)
+        elsif ret[:status_code] == 404
+          update_cache(app_uuid, nil, :delete)
+        end
+      end
+      
+      # Cache for a signer with a given app_uuid expires every minute
+      def signer_expired?(app_uuid)
+        signer = cached_signer(app_uuid)
+        signer.nil? || signer[:last_refresh] < (Time.now - 60)
+      end
+      
+      # Fetch signer from cache
+      def cached_signer(app_uuid)
+        synchronize { @cached_signers[app_uuid] }
+      end
+      
+      # Find the MAuth::Signer for app with given app_uuid
+      # Find either in cache or generate with remote data
+      def signer_for_app(app_uuid)
+        refresh_signer(app_uuid) if signer_expired?(app_uuid)
+        signer = cached_signer(app_uuid)
+        
+        if signer
+          return signer[:signer] # cached signers are actually hashes of the form {:signer => the_signer, :last_refresh => the time of last refresh}
+        else
+          log("Cannot find public key for app with uuid #{app_uuid} locally or in MAuth")
+          return nil
+        end
+      end
+      
       # Rack-mauth does its own authentication
       def authenticate_locally(digest, params)
-        public_key = public_key_for_app(params[:app_uuid])
-        public_key && MAuth::Signer.new(:public_key => public_key).verify_request(digest, params)
+        signer = signer_for_app(params[:app_uuid])
+        signer && signer.verify_request(digest, params)
       end
 
       # Ask mAuth to authenticate
@@ -226,17 +211,46 @@ module Medidata
       def unauthenticated_response
         [401, {'Content-Type' => 'text/plain'}, ['Unauthorized']]
       end
-
-      # Can we write to the Rails log
-      def can_log?
-        @can_log ||= (defined?(Rails) && Rails.respond_to?(:logger))
+      
+      # Returns status code and relevant response.body info given respose
+      def according_to(response)
+        return {:status_code => 500, :body => nil}  if response.nil?
+        
+        code = response.code.to_i
+        if code == 200
+          return {:status_code => 200, :body => response.body}
+        elsif code == 404
+          return {:status_code => 404, :body => nil}
+        elsif code >= 500
+          return {:status_code => 500, :body => nil}
+        else
+          log "Attempt to refresh cache with secret from mAuth responded with #{response.code.to_i} #{response.body}"
+          return {:status_code => 500, :body => nil}
+        end
       end
-
-      # Write to log
-      def log(str_to_log)
-        Rails.logger.info("rack-mauth: " + str_to_log) if can_log?
+      
+      # Get remote security token from MAuth (for the purposes of local authentication)
+      def get_remote_security_token(app_uuid)
+        headers = @mauth_signer_for_self.signed_request_headers(:app_uuid => @self_app_uuid, :verb => 'GET', :request_url => security_token_path(app_uuid))
+        response = get(security_token_url(app_uuid), {:headers => headers})
+        ret = according_to(response)
+        begin
+          ret[:body] = JSON.parse(ret[:body]) unless ret[:body].nil?
+        rescue JSON::ParserError, TypeError
+          ret = {:status_code => 500, :body => nil}
+          log "Cannot parse JSON response from MAuth for security token for app_uuid #{app_uuid} request "
+        end
+        
+        return ret
       end
-
+      
+      # Get remote public key from MAuth (for the purposes of local authentication)
+      def get_remote_public_key(app_uuid)
+        ret = get_remote_security_token(app_uuid)
+        pub_key = ret[:body].nil? ? nil : ret[:body]['security_token']['public_key_str']
+        {:status_code => ret[:status_code], :public_key => pub_key}
+      end
+      
       # Generic get
       def get(from_url, options = {})
         begin
@@ -274,5 +288,15 @@ module Medidata
         end
       end
 
+      # Can we write to the Rails log
+      def can_log?
+        @can_log ||= (defined?(Rails) && Rails.respond_to?(:logger))
+      end
+
+      # Write to log
+      def log(str_to_log)
+        Rails.logger.info("rack-mauth: " + str_to_log) if can_log?
+      end
+      
     end
 end
--- smart_app.ru
+++ smart_app.ru
@@ -3,12 +3,12 @@ require "bundler/setup"
 require 'rack'
 require 'rack/mauth'
 
-smart_app = lambda { |env| [200, {'Content-Type' => 'application/json'}, "Smart app: Success!\n"] }
+smart_app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Smart app: Success!\n"]] }
 
 config = {
-  :mauth_baseurl => 'https://mauth-sandbox.imedidata.net',
-  :private_key => 'QpeIisdvU9AJC15wkLf83xDPiCFaTL6r8iQY56nZqaqp0yTfJVKAgtOqdGyAOyVDd9b2Uz8ZukiyXR5H56JLEg==',
-  :app_uuid => '91111111-1111-1111-1111-111111111111',
+  :mauth_baseurl => 'http://0.0.0.0:3000',
+  :private_key => "-----BEGIN RSA PRIVATE KEY-----\nMIIEogIBAAKCAQEApr1NrmTQPlwZ4XKSl3bmmDCu0j7ME1goC5YD/XAHqe5pkrEV\nVk6j8gP+Gi/3UypGM/JmzWGSTenDFApkys3VkO7kwOqcHVO1wXTZTKLSUheR3H1p\nN5ZyfUkHrw/LTOm+tA0tNnqQF2FjdGLnY8Pni/3IjyaocKo2i+oKFaxdMO6M9dF+\neQEgVloQcSFPi5gNwgTvAu2urfJKMx+p5LU44A1kqf9NT86sIscF84PssPGHm5IL\n7mFXBmwPFrr4NODp1BsXchFPVIVGVFfHjYnwQLuyFTwfCi2LTr9o7MMuXAJUdtQd\n9ihiuSJwWR+PTkmeNVCs1s30ejHUv3PDCBKXSwIDAQABAoIBADhBQBcpfjS74CN3\nA0xE2lHYgvL+Kt4P7RrTly8HgB5uCIJsanV+/MMnY7C0JC6T4bGfA94hIDpXNvDo\n/M6LmZVXdCg+P0OJvZWydanseScnOpf4W+pcQO5SGFyQ6JdfeW7Hz0xFF547xlE7\nGTRIoTNTATqC4Wt5kgOsh5B+Ycai+Yve89DT2DHHG0xDk6iMEgSO4iFSQhDnoIBJ\nGaJPU2r79q3AGVrHJZUQGmeiKxDNwjxjY9EEGXGTFbADf8lhK7qh/eTJeIRsQ1Wi\nZ1gCVxnzqJdIO7kfZHiVXocpePf0WTfZvM4XqXhUt6EiOXR5TaT5vB1BuNNmBvXQ\nxntDxBECgYEA3DoBNYVr74jsMbcUB1iRHzgSLMw2Z0pQ19SZeF91/tTU6PxGQwaa\nI7etgSzaXFmZ7B6IC64muiu00aQjltnBCIKqMyfOHIOTs79Xordoq5LUWuV0EC1z\nOXpSN4tADkpgHh+qr8yn55Nf+hnR0HpRE+OkTTW+VNN8Ooz80j1APnkCgYEAwdMR\nZDJrlEjYu82ql641P6WNHpSKBCR/6Y27aw7i1n44UDfika4ODiEwkJ+Rjf2+FVVC\n9kz9I9tkjBtoP7BN0ffRvyx607dV9MmRRhJrhDFLAkNZqYobMnE3ihXB+AcDU3rY\neD+cQAHmigwbkkSam2ek1dQZOIZomtXyw2gAQuMCgYAftG4WIXYvjvvKEHxerl5+\nKxlav6+ZYTaQS/goPz4CiOt5+0+2OI4aVEgzT5zELNYfCyo03EaRCNfIUqQZBJJo\nwj70jGd87WhnOUXJlDQKd2IBEAWMiq6K+NQ7UN3Q8N4zmAV/t6v4h9wKaostQ17G\nyUAPKYyUM7ovx7piHhVQqQKBgBls66oeJxiTmcLBDvDIzHll6SYqzBQRCaqEiiJY\nGI+UjSSQwCrmDzfxSKKgHALpV0cLITaYENjkTcNHURyRrxOtE5mlZxNgyGjNDD6J\n6gq0QKeyWA+yazDpwyRdCE3V9ay8v6q+hWusFCblwbQlRba/GNNn+Er+7rfo+uiB\nOw+LAoGARyRQ6azgTT1USjcENcWJYa9sUUQuAwJHfuay1R4j0ufinM/VciPAF08R\nLwT6kd5AqDZ7xyebqp7VrCwD7ttXEj5u+oDxUSUx+v2JV1nUggEoqejPuVGWg5ce\n2XHbr5ULf9PJioDJSs113I8kc7TaXmrNqS5LqMdaxev/XiC/tjg=\n-----END RSA PRIVATE KEY-----\n",
+  :app_uuid => 'A',
   :version => 'v1'
 }
 
