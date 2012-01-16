824bdb05d92de290b4f31384c75175b2d9fb3681
Matthew Szenher <mszenher@mdsol.com>
2012-01-16 17:05:30 -0500

    make integration tests work on new mauth sandbox

--- spec/integration/rack-mauth_spec.rb
+++ spec/integration/rack-mauth_spec.rb
@@ -1,8 +1,12 @@
 require 'rack/test'
 require 'rack/mauth'
 
-APP_UUID = 'A'
-PRIVATE_KEY = "-----BEGIN RSA PRIVATE KEY-----\nMIIEogIBAAKCAQEApr1NrmTQPlwZ4XKSl3bmmDCu0j7ME1goC5YD/XAHqe5pkrEV\nVk6j8gP+Gi/3UypGM/JmzWGSTenDFApkys3VkO7kwOqcHVO1wXTZTKLSUheR3H1p\nN5ZyfUkHrw/LTOm+tA0tNnqQF2FjdGLnY8Pni/3IjyaocKo2i+oKFaxdMO6M9dF+\neQEgVloQcSFPi5gNwgTvAu2urfJKMx+p5LU44A1kqf9NT86sIscF84PssPGHm5IL\n7mFXBmwPFrr4NODp1BsXchFPVIVGVFfHjYnwQLuyFTwfCi2LTr9o7MMuXAJUdtQd\n9ihiuSJwWR+PTkmeNVCs1s30ejHUv3PDCBKXSwIDAQABAoIBADhBQBcpfjS74CN3\nA0xE2lHYgvL+Kt4P7RrTly8HgB5uCIJsanV+/MMnY7C0JC6T4bGfA94hIDpXNvDo\n/M6LmZVXdCg+P0OJvZWydanseScnOpf4W+pcQO5SGFyQ6JdfeW7Hz0xFF547xlE7\nGTRIoTNTATqC4Wt5kgOsh5B+Ycai+Yve89DT2DHHG0xDk6iMEgSO4iFSQhDnoIBJ\nGaJPU2r79q3AGVrHJZUQGmeiKxDNwjxjY9EEGXGTFbADf8lhK7qh/eTJeIRsQ1Wi\nZ1gCVxnzqJdIO7kfZHiVXocpePf0WTfZvM4XqXhUt6EiOXR5TaT5vB1BuNNmBvXQ\nxntDxBECgYEA3DoBNYVr74jsMbcUB1iRHzgSLMw2Z0pQ19SZeF91/tTU6PxGQwaa\nI7etgSzaXFmZ7B6IC64muiu00aQjltnBCIKqMyfOHIOTs79Xordoq5LUWuV0EC1z\nOXpSN4tADkpgHh+qr8yn55Nf+hnR0HpRE+OkTTW+VNN8Ooz80j1APnkCgYEAwdMR\nZDJrlEjYu82ql641P6WNHpSKBCR/6Y27aw7i1n44UDfika4ODiEwkJ+Rjf2+FVVC\n9kz9I9tkjBtoP7BN0ffRvyx607dV9MmRRhJrhDFLAkNZqYobMnE3ihXB+AcDU3rY\neD+cQAHmigwbkkSam2ek1dQZOIZomtXyw2gAQuMCgYAftG4WIXYvjvvKEHxerl5+\nKxlav6+ZYTaQS/goPz4CiOt5+0+2OI4aVEgzT5zELNYfCyo03EaRCNfIUqQZBJJo\nwj70jGd87WhnOUXJlDQKd2IBEAWMiq6K+NQ7UN3Q8N4zmAV/t6v4h9wKaostQ17G\nyUAPKYyUM7ovx7piHhVQqQKBgBls66oeJxiTmcLBDvDIzHll6SYqzBQRCaqEiiJY\nGI+UjSSQwCrmDzfxSKKgHALpV0cLITaYENjkTcNHURyRrxOtE5mlZxNgyGjNDD6J\n6gq0QKeyWA+yazDpwyRdCE3V9ay8v6q+hWusFCblwbQlRba/GNNn+Er+7rfo+uiB\nOw+LAoGARyRQ6azgTT1USjcENcWJYa9sUUQuAwJHfuay1R4j0ufinM/VciPAF08R\nLwT6kd5AqDZ7xyebqp7VrCwD7ttXEj5u+oDxUSUx+v2JV1nUggEoqejPuVGWg5ce\n2XHbr5ULf9PJioDJSs113I8kc7TaXmrNqS5LqMdaxev/XiC/tjg=\n-----END RSA PRIVATE KEY-----\n"
+# NOTE:  In order for these tests to pass, the APP_UUID and PUBLIC_KEY given below must exist as a security token
+# in the persistent store of the specified MAUTH_BASE_URL
+MAUTH_BASE_URL = "https://mauth-sandbox.imedidata.net"
+APP_UUID = "testing_hands_off"
+PRIVATE_KEY = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEA2GhmijLqVmuT2D7H0wLfq4LIJ6fpHMyl5Tz1mytNz4/pa0fI\nzxabMDBpC9hhLkzNUPoOFcI9Cz2aEIW+HHqkxCcifmqXEF5MlQz1OHJYKKFXvEPu\nWNlILCtCiRosw5xwZDrVntufPhsUWTTiHpG9VHdK79VnUVD65yA8f9ma9Gwsx9/A\nRpetEpzttm+183nY6adGytmK32F8l58DmGyV5lGHcouyhddMwLD2VDJm6hlMqlN7\nJrn7YdzcHCWrUUkan7zwlS/d/AGN9gjFHKa6dJvyTAONTD4s2SHNifhv79eeu1Az\nNlTIb9X0ZgRPPRh/6NfVLsmj6ScVloW2Bf7VHQIDAQABAoIBAHJT3XA/a9vSI55H\ntIu+5emXQyToKVhkqXQNG3gpjYcNcXSSzPzS5ZO0z5pJazXpr0KLiGtoXZWVqtH1\nxjUHegqC3k9JApvMJctMuDRk/Dwi2NYGUWIxEFb9V75UzLde62WYS4kMX/mQltR2\nAsvBlPONvlIsPNQR4yu9tRiaHqnnj1ITUWLhndfjQBTEgqXF948m2Ltc6ub/cHj3\nsDglxyjteH9QyiZUk7hUVOpMla16uFosOX5crsoMNrul6dHLRcTcr4YVutAQ0/uF\nMKB06GiOWMIOUPDvd5rYcrsamy2IcZaz3M/SKmePBPEF/nbM0dVS3yfbj4uCQXZ9\nSQmb4oUCgYEA933iN5pXYp6dZmEyjrvcFRNpw2pbIq6KU+2sg5YEL3c4rpA7ABbT\nEOhUUIg8uJM+AcGQSp1EoBPQLXFbCwxftqz/vKuXxR458GuTOGCVIQh++RE1aunK\nEJ84q4Uta6O3XLHIRvhMTsbV4iVQTblRqPRd7+z8KJNGXVzs43cF93MCgYEA39j0\nZD3Soi2otZi9tzvuBgxzFyhn8Tkcm0YuBvUMCGweusjxzMUbJ5JbFfFzWxVKrQ6Z\nrrtwqQ+54RonciuSbwQTEMznYCcX3IFGTxCCx3fg9GfoWmvbCYKGq8sJXD1e2qOy\ny+GlaOKnrjo0UQm5GNk/GlUpfSV+UGTYjeRQPS8CgYBX0rvrr0FDJbYFFoiyTceT\nUwg86AjfDcDYd4a4SwvBLDVY/KVzKqZLYaZJzY5+kQF37hAd6iDoDR/agFcmXIW6\ndTlq4hlBQbCduA7N+rfwuOsVxx2FiuDBdT7O3rt3bukqY4wGYyXw7m4HieYtLo3j\nvpN3CEmSvHBDwS3uqdXcMQKBgHDfHuxk2A764vUenZsFVxIpuObWcwMJf0k0bAUK\nDxU4H46jwHk2cmjTvaYk57vn0o3MrOWUkkxNJ7c/zuAc5GuiLFLuX0T2sWt4rBE2\nDBu0cPQMaPcfJ4V2EZ4SdRfTwj6RCJkRoKxwjYimxLaQJotHEDCg/JikDTtQfnmd\nxG/1AoGBAOcgAo/ROELsh/DZEcHYLUSeCUYU7bMBzASYNZPwyJsIgLSWgiRFUD3I\nN33J/5/IkkupRzXo1DwOQO5JF2a3OPEY3LENq7YmjzHj1/KAfehvl+K1qtt7Ian5\nPdfW9FmUnq0G0+Y0JCodMk1g9u9h3klTZ6ah3PHlcwmNjmKRVX3n\n-----END RSA PRIVATE KEY-----\n"
+PUBLIC_KEY = "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEA2GhmijLqVmuT2D7H0wLfq4LIJ6fpHMyl5Tz1mytNz4/pa0fIzxab\nMDBpC9hhLkzNUPoOFcI9Cz2aEIW+HHqkxCcifmqXEF5MlQz1OHJYKKFXvEPuWNlI\nLCtCiRosw5xwZDrVntufPhsUWTTiHpG9VHdK79VnUVD65yA8f9ma9Gwsx9/ARpet\nEpzttm+183nY6adGytmK32F8l58DmGyV5lGHcouyhddMwLD2VDJm6hlMqlN7Jrn7\nYdzcHCWrUUkan7zwlS/d/AGN9gjFHKa6dJvyTAONTD4s2SHNifhv79eeu1AzNlTI\nb9X0ZgRPPRh/6NfVLsmj6ScVloW2Bf7VHQIDAQAB\n-----END RSA PUBLIC KEY-----\n"
 
 describe 'Local Authentication with Rack-Mauth' do
   include Rack::Test::Methods
@@ -11,7 +15,7 @@ describe 'Local Authentication with Rack-Mauth' do
     mini_app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Hello World"]] }
     
     config = {
-      :mauth_baseurl => 'http://0.0.0.0:3000',
+      :mauth_baseurl => MAUTH_BASE_URL,
       :private_key => PRIVATE_KEY,
       :app_uuid => APP_UUID,
       :mauth_api_version => 'v1'
