029a55304c2c30bc1a445cdca528833bf89eb9d3
Ethan <ethan@medidata>
2012-02-15 11:56:13 -0500

    mauth client spec, round 2: authenticators

--- spec/mauth_client_spec.rb
+++ spec/mauth_client_spec.rb
@@ -56,9 +56,16 @@ describe MAuth::Client do
 
   require 'mauth/request_and_response'
   class TestSignableRequest < MAuth::Request
+    include MAuth::Signed
     attr_accessor :headers
     def merge_headers(headers)
       self.class.new(@attributes_for_signing).tap{|r| r.headers = (@headers || {}).merge(headers) }
+    end
+    def x_mws_time
+      headers['X-MWS-Time']
+    end
+    def x_mws_authentication
+      headers['X-MWS-Authentication']
     end
   end
 
@@ -85,6 +92,124 @@ describe MAuth::Client do
       signed_headers = mc.signed_headers(TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom'))
       assert signed_headers.keys.include?('X-MWS-Authentication')
       assert signed_headers.keys.include?('X-MWS-Time')
+    end
+  end
+
+  describe 'authenticators' do
+    before do
+      @signing_key = OpenSSL::PKey::RSA.generate(2048)
+      @app_uuid = 'signer'
+      @signing_mc = MAuth::Client.new(:private_key => @signing_key, :app_uuid => @app_uuid)
+    end
+
+    shared_examples MAuth::Client::Authenticator do
+      it "considers an authentically-signed request to be inauthentic when it's too old or too far in the future" do
+        request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+        {-301 => false, -299 => true, 299 => true, 301 => false}.each do |time_offset, authentic|
+          signed_request = @signing_mc.signed(request, :time => Time.now.to_i + time_offset)
+          assert_equal authentic, @authenticating_mc.authentic?(signed_request)
+        end
+      end
+      it "considers an authentically-signed request to be inauthentic when it has no x-mws-time" do
+        request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+        signed_request = @signing_mc.signed(request)
+        signed_request.headers.delete('X-MWS-Time')
+        assert !@authenticating_mc.authentic?(signed_request)
+      end
+      it "considers a request with a bad MWS token to be inauthentic" do
+        request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+        ['mws', 'm.w.s', 'm w s', 'NWS', ' MWS'].each do |bad_token|
+          signed_request = @signing_mc.signed(request)
+          signed_request.headers['X-MWS-Authentication'] = signed_request.headers['X-MWS-Authentication'].sub(/\AMWS/, bad_token)
+          assert !@authenticating_mc.authentic?(signed_request)
+        end
+      end
+      it "raises UnableToAuthenticate if mauth is down" do
+        @test_faraday.stub(:get).and_raise(::Faraday::Error::ConnectionFailed.new(''))
+        @test_faraday.stub(:post).and_raise(::Faraday::Error::ConnectionFailed.new(''))
+        request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+        signed_request = @signing_mc.signed(request)
+        assert_raises(MAuth::UnableToAuthenticateError) { @authenticating_mc.authentic?(signed_request) }
+      end
+      it "raises UnableToAuthenticate if mauth errors" do
+        @stubs.instance_eval{ @stack.clear } #HAX 
+        @stubs.get("/mauth/v1/security_tokens/#{@app_uuid}.json") { [500, {}, []] } # for the local authenticator 
+        @stubs.post('/mauth/v1/authentication_tickets.json') { [500, {}, []] } # for the remote authenticator 
+        request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+        signed_request = @signing_mc.signed(request)
+        assert_raises(MAuth::UnableToAuthenticateError) { @authenticating_mc.authentic?(signed_request) }
+      end
+    end
+
+    describe MAuth::Client::LocalAuthenticator do
+      describe '#authentic?' do
+        before do
+          @authenticating_mc = MAuth::Client.new(:mauth_baseurl => 'http://whatever', :mauth_api_version => 'v1', :private_key => OpenSSL::PKey::RSA.generate(2048), :app_uuid => 'authenticator')
+          assert @authenticating_mc.is_a?(MAuth::Client::LocalAuthenticator)
+          require 'faraday'
+          stubs = @stubs = Faraday::Adapter::Test::Stubs.new
+          @test_faraday = ::Faraday.new do |builder|
+            builder.adapter(:test, stubs) do |stub|
+              stub.get("/mauth/v1/security_tokens/#{@app_uuid}.json") { [200, {}, JSON.generate({'security_token' => {'public_key_str' => @signing_key.public_key.to_s}})] }
+            end
+          end
+          ::Faraday.stub(:new).and_return(@test_faraday)
+        end
+        include_examples MAuth::Client::Authenticator
+        it 'considers an authentically-signed request to be authentic' do
+          request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+          signed_request = @signing_mc.signed(request)
+          assert @authenticating_mc.authentic?(signed_request)
+        end
+        it 'considers a request signed by an app uuid unknown to mauth to be inauthentic' do
+          request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+          signing_mc = MAuth::Client.new(:private_key => @signing_key, :app_uuid => 'nope')
+          @stubs.get("/mauth/v1/security_tokens/nope.json") { [404, {}, []] }
+          signed_request = signing_mc.signed(request)
+          assert !@authenticating_mc.authentic?(signed_request)
+        end
+        it "considers a request with a bad signature to be inauthentic" do
+          request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+          signed_request = @signing_mc.signed(request)
+          signed_request.headers['X-MWS-Authentication'] = "MWS #{@app_uuid}:wat"
+          assert !@authenticating_mc.authentic?(signed_request)
+        end
+        it "considers a request that has been tampered with to be inauthentic" do
+          request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+          signed_request = @signing_mc.signed(request)
+          signed_request.attributes_for_signing[:verb] = 'DELETE'
+          assert !@authenticating_mc.authentic?(signed_request)
+        end
+      end
+    end
+
+    describe MAuth::Client::RemoteRequestAuthenticator do
+      describe '#authentic?' do
+        before do
+          @authenticating_mc = MAuth::Client.new(:mauth_baseurl => 'http://whatever', :mauth_api_version => 'v1')
+          assert @authenticating_mc.is_a?(MAuth::Client::RemoteRequestAuthenticator)
+          require 'faraday'
+          stubs = @stubs = Faraday::Adapter::Test::Stubs.new
+          @test_faraday = ::Faraday.new do |builder|
+            builder.adapter(:test, stubs)
+          end
+          @stubs.post('/mauth/v1/authentication_tickets.json') { [204, {}, []] }
+          ::Faraday.stub(:new).and_return(@test_faraday)
+        end
+        include_examples MAuth::Client::Authenticator
+        it 'considers a request to be authentic if mauth reports it so' do
+          request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+          signed_request = @signing_mc.signed(request)
+          assert @authenticating_mc.authentic?(signed_request)
+        end
+        it 'considers a request to be inauthentic if mauth reports it so' do
+          @stubs.instance_eval{ @stack.clear } #HAX 
+          @stubs.post('/mauth/v1/authentication_tickets.json') { [412, {}, []] }
+          request = TestSignableRequest.new(:verb => 'PUT', :request_url => '/', :body => 'himom')
+          signed_request = @signing_mc.signed(request)
+          assert !@authenticating_mc.authentic?(signed_request)
+        end
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'timecop'
 require 'uuidtools'
+require 'json'
+
 if (RUBY_VERSION.split('.').map(&:to_i) <=> [1, 9]) >= 0
   require 'simplecov'
   require 'simplecov-gem-adapter'
