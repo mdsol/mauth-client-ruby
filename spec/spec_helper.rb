f3c0334a4b7102e45e1eae28c686ba9e899bbf7a
Ethan <ethan@medidata>
2012-02-15 06:37:31 -0500

    make rspec suck less - use test unit assertions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,6 @@ if (RUBY_VERSION.split('.').map(&:to_i) <=> [1, 9]) >= 0
   require 'simplecov-gem-adapter'
   SimpleCov.start 'gem'
 end
+
+require 'test/unit/assertions'
+RSpec::Core::ExampleGroup.send(:include, Test::Unit::Assertions)
