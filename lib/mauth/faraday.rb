c7812cc04b5d151f4a812614eb3f1782d1221ee3
Jordi Polo Carres <jcarres@mdsol.com>
2016-03-01 16:12:54 +0900

    syntax problems solved by rubocop -a

--- lib/mauth/faraday.rb
+++ lib/mauth/faraday.rb
@@ -1,14 +1,12 @@
 require 'mauth/middleware'
 require 'mauth/request_and_response'
 
-
-Faraday::Request.register_middleware(:mauth_request_signer => proc { MAuth::Faraday::RequestSigner })
-Faraday::Response.register_middleware(:mauth_response_authenticator => proc { MAuth::Faraday::ResponseAuthenticator })
-
+Faraday::Request.register_middleware(mauth_request_signer: proc { MAuth::Faraday::RequestSigner })
+Faraday::Response.register_middleware(mauth_response_authenticator: proc { MAuth::Faraday::ResponseAuthenticator })
 
 module MAuth
   module Faraday
-    # faraday middleware to sign outgoing requests 
+    # faraday middleware to sign outgoing requests
     class RequestSigner < MAuth::Middleware
       def call(request_env)
         signed_request_env = mauth_client.signed(MAuth::Faraday::Request.new(request_env)).request_env
@@ -16,50 +14,55 @@ module MAuth
       end
     end
 
-    # faraday middleware to authenticate incoming responses 
+    # faraday middleware to authenticate incoming responses
     class ResponseAuthenticator < MAuth::Middleware
       def call(request_env)
         @app.call(request_env).on_complete do |response_env|
           mauth_response = MAuth::Faraday::Response.new(response_env)
-          mauth_client.authenticate!(mauth_response) # raises MAuth::InauthenticError when inauthentic 
+          mauth_client.authenticate!(mauth_response) # raises MAuth::InauthenticError when inauthentic
           response_env.merge('mauth.app_uuid' => mauth_response.signature_app_uuid, 'mauth.authentic' => true)
         end
       end
     end
 
-    # representation of a request (outgoing) composed from a Faraday request env which can be 
-    # passed to a Mauth::Client for signing 
+    # representation of a request (outgoing) composed from a Faraday request env which can be
+    # passed to a Mauth::Client for signing
     class Request < MAuth::Request
       attr_reader :request_env
       def initialize(request_env)
         @request_env = request_env
       end
+
       def attributes_for_signing
         request_url = @request_env[:url].path
         request_url = '/' if request_url.empty?
-        @attributes_for_signing ||= {:verb => @request_env[:method].to_s.upcase, :request_url => request_url, :body => @request_env[:body]}
+        @attributes_for_signing ||= { verb: @request_env[:method].to_s.upcase, request_url: request_url, body: @request_env[:body] }
       end
-      # takes a Hash of headers; returns an instance of this class whose 
+
+      # takes a Hash of headers; returns an instance of this class whose
       # headers have been merged with the argument headers
       def merge_headers(headers)
-        self.class.new(@request_env.merge(:request_headers => @request_env[:request_headers].merge(headers)))
+        self.class.new(@request_env.merge(request_headers: @request_env[:request_headers].merge(headers)))
       end
     end
 
-    # representation of a Response (incoming) composed from a Faraday response env which can be 
-    # passed to a Mauth::Client for authentication 
+    # representation of a Response (incoming) composed from a Faraday response env which can be
+    # passed to a Mauth::Client for authentication
     class Response < MAuth::Response
       include Signed
       attr_reader :response_env
       def initialize(response_env)
         @response_env = response_env
       end
+
       def attributes_for_signing
-        @attributes_for_signing ||= {:status_code => response_env[:status], :body => response_env[:body]}
+        @attributes_for_signing ||= { status_code: response_env[:status], body: response_env[:body] }
       end
+
       def x_mws_time
         @response_env[:response_headers]['x-mws-time']
       end
+
       def x_mws_authentication
         @response_env[:response_headers]['x-mws-authentication']
       end
@@ -67,10 +70,11 @@ module MAuth
 
     # add MAuth-Client's user-agent to a request
     class MAuthClientUserAgent
-      def initialize(app, agent_base="Mauth-Client")
+      def initialize(app, agent_base = "Mauth-Client")
         @app = app
         @agent_base = agent_base
       end
+
       def call(request_env)
         agent = "#{@agent_base} (MAuth-Client: #{MAuth::VERSION}; Ruby: #{RUBY_VERSION}; platform: #{RUBY_PLATFORM})"
         request_env[:request_headers]['User-Agent'] ||= agent
--- lib/mauth/proxy.rb
+++ lib/mauth/proxy.rb
@@ -3,23 +3,23 @@ require 'faraday'
 require 'rack'
 
 module MAuth
-  # MAuth::Proxy is a simple Rack application to take incoming requests, sign them with MAuth, and 
-  # proxy them to a target URI. the responses from the target may be authenticated, with MAuth 
+  # MAuth::Proxy is a simple Rack application to take incoming requests, sign them with MAuth, and
+  # proxy them to a target URI. the responses from the target may be authenticated, with MAuth
   # (and are by default).
   class Proxy
-    # target_uri is the base relative to which requests are made. 
+    # target_uri is the base relative to which requests are made.
     #
     # options:
-    # - :authenticate_responses - boolean, default true. whether responses will be authenticated. 
-    #   if this is true and an inauthentic response is encountered, then MAuth::InauthenticError 
+    # - :authenticate_responses - boolean, default true. whether responses will be authenticated.
+    #   if this is true and an inauthentic response is encountered, then MAuth::InauthenticError
     #   will be raised.
-    # - :mauth_config - configuration passed to MAuth::Client.new (see its doc). default is 
+    # - :mauth_config - configuration passed to MAuth::Client.new (see its doc). default is
     #   MAuth::Client.default_config
-    def initialize(target_uri, options={})
+    def initialize(target_uri, options = {})
       @target_uris = target_uri
       @browser_proxy = options.delete(:browser_proxy)
       @options = options
-      options = {:authenticate_responses => true}.merge(options)
+      options = { authenticate_responses: true }.merge(options)
       options[:mauth_config] ||= MAuth::Client.default_config
       if @browser_proxy # Browser proxy mode
         @signer_connection = ::Faraday.new do |builder|
@@ -54,8 +54,8 @@ module MAuth
       request_body = request_env['rack.input'].read
       request_env['rack.input'].rewind
       request_headers = {}
-      request_env.each do |k,v|
-        if k =~ /\AHTTP_/ && !%w(HTTP_HOST).include?(k)
+      request_env.each do |k, v|
+        if k.start_with?('HTTP_') && !%w(HTTP_HOST).include?(k)
           name = $'
           request_headers[name] = v
         end
@@ -63,12 +63,12 @@ module MAuth
       request_headers.merge!(@persistent_headers)
       if @browser_proxy
         target_uri = request_env["REQUEST_URI"]
-        connection = @target_uris.any? { |u| target_uri.start_with? u} ? @signer_connection :  @unsigned_connection
+        connection = @target_uris.any? { |u| target_uri.start_with? u } ? @signer_connection : @unsigned_connection
         response = connection.run_request(request_method, target_uri, request_body, request_headers)
       else
         response = @connection.run_request(request_method, request.fullpath, request_body, request_headers)
       end
-      response_headers = response.headers.reject do |name, value|
+      response_headers = response.headers.reject do |name, _value|
         %w(Content-Length Transfer-Encoding).map(&:downcase).include?(name.downcase)
       end
       [response.status, response_headers, [response.body || '']]
