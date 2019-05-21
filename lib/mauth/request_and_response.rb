52580385c0d9a9eaee9008e597821806f8ccb69d
Cameron McInnes <cmcinnes@mdsol.com>
2019-05-21 11:45:12 -0400

    update comments, add query string encoding helpers and v2 string to sign

--- lib/mauth/request_and_response.rb
+++ lib/mauth/request_and_response.rb
@@ -4,7 +4,7 @@ module MAuth
   # module which composes a string to sign.
   #
   # includer must provide
-  # - SIGNATURE_COMPONENTS constant - array of keys to get from #attributes_for_signing
+  # - SIGNATURE_COMPONENTS OR SIGNATURE_COMPONENTS_V2 constant - array of keys to get from #attributes_for_signing
   # - #attributes_for_signing
   # - #merge_headers (takes a Hash of headers; returns an instance of includer's own class whose
   #   headers have been updated with the argument headers)
@@ -12,6 +12,22 @@ module MAuth
     # composes a string suitable for private-key signing from the SIGNATURE_COMPONENTS keys of
     # attributes for signing, which are themselves taken from #attributes_for_signing and
     # the given argument more_attributes
+
+    # The string to sign for V1 protocol will be (where LF is line feed character)
+    # For requests:
+    #   string_to_sign =
+    #     http_verb + <LF> +
+    #     resource_url_path (no host, port or query string; first "/" is included) + <LF> +
+    #     request_body + <LF> +
+    #     app_uuid + <LF> +
+    #     current_seconds_since_epoch + <LF> +
+    #
+    # For responses:
+    #   string_to_sign =
+    #     status_code_string + <LF> +
+    #     response_body_digest + <LF> +
+    #     app_uuid + <LF> +
+    #     current_seconds_since_epoch
     def string_to_sign(more_attributes)
       attributes_for_signing = self.attributes_for_signing.merge(more_attributes)
       missing_attributes = self.class::SIGNATURE_COMPONENTS.select { |key| !attributes_for_signing.key?(key) || attributes_for_signing[key].nil? }
@@ -21,6 +37,66 @@ module MAuth
       end
       string = self.class::SIGNATURE_COMPONENTS.map { |k| attributes_for_signing[k].to_s }.join("\n")
       Digest::SHA512.hexdigest(string)
+    end
+
+    # The string to sign for V2 protocol will be (where LF is line feed character)
+    # For requests:
+    #   string_to_sign =
+    #     http_verb + <LF> +
+    #     resource_url_path (no host, port or query string; first "/" is included) + <LF> +
+    #     request_body_digest + <LF> +
+    #     app_uuid + <LF> +
+    #     current_seconds_since_epoch + <LF> +
+    #     encoded_query_params
+    #
+    # For responses:
+    #   string_to_sign =
+    #     status_code_string + <LF> +
+    #     response_body_digest + <LF> +
+    #     app_uuid + <LF> +
+    #     current_seconds_since_epoch
+    def string_to_sign_v2(more_attributes)
+      attributes_for_signing = self.attributes_for_signing.merge(more_attributes)
+
+      # where to make the digest of the body? shouldn't be here because we have to
+      # call string to sign thrice and that's wasted hash power
+      # is this or equals dumb?
+      attributes_for_signing[:request_body_digest] ||= Digest::SHA512.hexdigest(attributes_for_signing[:body])
+      attributes_for_signing[:encoded_query_params] ||= encode_query_string(attributes_for_signing[:query_string])
+
+      missing_attributes = self.class::SIGNATURE_COMPONENTS_V2.reject do |key|
+        attributes_for_signing.dig(key)
+      end
+
+      missing_attributes.delete(:body) # body may be omitted
+      if missing_attributes.any?
+        raise(UnableToSignError, "Missing required attributes to sign: #{missing_attributes.inspect}\non object to sign: #{inspect}")
+      end
+
+      string = self.class::SIGNATURE_COMPONENTS.map do |k|
+        attributes_for_signing[k].to_s.force_encoding(CHARACTER_ENCODING)
+      end.join("\n")
+      Digest::SHA512.hexdigest(string)
+    end
+
+    # todo how to handle array query params -> do rack and faraday handle them in the same way?
+
+    # Sorts query string parameters by codepoint, uri encodes keys and values,
+    # and rejoins parameters into a query string
+    def encode_query_string(q_string)
+      q_string.split('&').sort.map do |part|
+        k, e, v = part.partition('=')
+        uri_escape(k) + e + uri_escape(v)
+      end.join('&')
+    end
+
+    # Percent encodes special characters, preserving character encoding, and
+    # encoding spaces as %20%
+    def uri_escape(string)
+      encoding = string.encoding
+      string.b.gsub(/([^a-zA-Z0-9_.~-]+)/) do |m|
+        '%' + m.unpack('H2' * m.bytesize).join('%').upcase
+      end.force_encoding(encoding)
     end
 
     def initialize(attributes_for_signing)
@@ -35,13 +111,28 @@ module MAuth
   # methods for an incoming object which is expected to have a signature.
   #
   # includer must provide
+  # - #mcc_authentication which returns that header's value
+  # - #mcc_time
+  # OR
   # - #x_mws_authentication which returns that header's value
   # - #x_mws_time
   module Signed
-    # returns a hash with keys :token, :app_uuid, and :signature parsed from the X-MWS-Authentication header
+    # mauth_client will authenticate with the highest protocol version present and ignore other
+    # protocol versions.
+    # returns a hash with keys :token, :app_uuid, and :signature parsed from the MCC-Authentication header
+    # if it is present and if not then the X-MWS-Authentication header if it is present.
+    # Note MWSV2 protocol no longer allows more than one space between the token app uuid.
     def signature_info
       @signature_info ||= begin
-        match = x_mws_authentication && x_mws_authentication.match(/\A([^ ]+) *([^:]+):([^:]+)\z/)
+        match = if mcc_authentication
+          mcc_authentication.match(
+            # QUESTION should this just use a general regex or use the token const
+            /\A([^ ]+) ([^:]+):([^:]+)#{MAuth::Client::AUTH_HEADER_DELIMITER}\z/
+          )
+        elsif x_mws_authentication
+          x_mws_authentication.match(/\A([^ ]+) *([^:]+):([^:]+)\z/)
+        end
+
         match ? { token: match[1], app_uuid: match[2], signature: match[3] } : {}
       end
     end
@@ -61,13 +152,15 @@ module MAuth
 
   # virtual base class for signable requests
   class Request
-    SIGNATURE_COMPONENTS = [:verb, :request_url, :body, :app_uuid, :time].freeze
+    SIGNATURE_COMPONENTS = %i[verb request_url body app_uuid time].freeze
+    SIGNATURE_COMPONENTS_V2 = %i[verb request_url request_body_digest app_uuid time encoded_query_params].freeze
     include Signable
   end
 
   # virtual base class for signable responses
   class Response
-    SIGNATURE_COMPONENTS = [:status_code, :body, :app_uuid, :time].freeze
+    SIGNATURE_COMPONENTS = %i[status_code body app_uuid time].freeze
+    SIGNATURE_COMPONENTS_V2 = %i[status_code body app_uuid time].freeze
     include Signable
   end
 end
