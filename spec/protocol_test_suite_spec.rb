c0400f024faf13a7345fcef1131e3c245d930ec6
Cameron McInnes <cmcinnes@mdsol.com>
2020-04-14 14:12:08 -0400

    add protocol test suite parser and spec file

---
+++ spec/protocol_test_suite_spec.rb
@@ -0,0 +1,89 @@
+require 'test_suite_parser'
+require 'faraday'
+require 'mauth/client'
+require 'byebug'
+
+describe 'MAuth Client passes the MWSV2 protocol test suite', integration:true do
+  let(:app_uuid) { ProtocolHelper::Config.app_uuid }
+  let(:request_time) { ProtocolHelper::Config.request_time }
+  let(:mauth_client) { ProtocolHelper::Config.mauth_client }
+
+  before(:all) { ProtocolHelper::Config.load }
+
+  ProtocolHelper::Config.cases.each do |case_dir|
+    context "#{case_dir}" do
+      let(:runner) { ProtocolHelper::CaseParser.new(case_dir) }
+      let(:req_attrs) { runner.req_attrs }
+      let(:uri_obj) { URI(req_attrs['url']) }
+      let(:expected_str_to_sign) { runner.sts }
+      let(:expected_signature) { runner.sig }
+      let(:expected_auth_headers) { runner.auth_headers }
+      let(:body) { runner.req_attrs['body'] }
+      let(:faraday_env) do
+        {
+          method: req_attrs['verb'],
+          url: uri_obj,
+          body: body
+        }
+      end
+      let(:faraday_req) { MAuth::Faraday::Request.new(faraday_env) }
+
+      unless case_dir.match?(/authentication-only/)
+        context 'signing' do
+          it 'generates the corect string to sign' do
+            signing_info = {
+              app_uuid: app_uuid,
+              time: request_time
+            }
+            sts = faraday_req.string_to_sign_v2(signing_info)
+            verb, path, body_digest, app_uuid, time, encoded_q_prms = sts.split("\n")
+            elements = sts.split("\n")
+            expected_elements = expected_str_to_sign.split("\n")
+
+            elements.zip(expected_elements).each do |duple|
+              expect(duple[0]).to eq(duple[1])
+            end
+            expect(faraday_req.string_to_sign_v2(signing_info)).to eq(expected_str_to_sign)
+          end
+
+          it 'generates the correct signature' do
+            sig = mauth_client.signature_v2(expected_str_to_sign)
+            expect(sig).to eq(expected_signature)
+          end
+
+          it 'generates the correct authentication headers' do
+            headers = mauth_client.signed_headers_v2(faraday_req, time: request_time)
+            expect(headers).to eq(expected_auth_headers)
+          end
+        end
+      end
+
+      context 'authentication' do
+        let(:pub_key) { ProtocolHelper::Config.pub_key }
+        let(:path) { req_attrs['url'].split('?')[0] }
+        let(:query) { req_attrs['url'].split('?')[1].to_s }
+        let(:rackified_auth_headers) do
+          expected_auth_headers.transform_keys! { |k| k.upcase.gsub('-','_').prepend('HTTP_') }
+        end
+        let(:mock_rack_env) do
+          {
+            'REQUEST_METHOD' => req_attrs['verb'],
+            'PATH_INFO' => path,
+            'QUERY_STRING' => query,
+            'rack.input' => double('rack.input', rewind: nil, read: body)
+          }.merge(rackified_auth_headers)
+        end
+
+        before do
+          allow(Time).to receive(:now).and_return(Time.at(request_time))
+          allow(mauth_client).to receive(:retrieve_public_key).and_return(pub_key)
+        end
+
+        it 'considers the authentically-signed request to be authentic' do
+          rack_req = MAuth::Rack::Request.new(mock_rack_env)
+          expect { mauth_client.authenticate!(rack_req) }.not_to raise_error
+        end
+      end
+    end
+  end
+end
---
+++ spec/test_suite_parser.rb
@@ -0,0 +1,65 @@
+require 'mauth/client'
+require 'faraday'
+
+module ProtocolHelper
+  TEST_SUITE_BASE_PATH = 'spec/fixtures/mauth-protocol-test-suite'.freeze
+  CASE_PATH = "#{TEST_SUITE_BASE_PATH}/protocols/MWSV2".freeze
+
+  class Config
+    class << self
+
+      attr_reader :request_time, :app_uuid, :mauth_client, :pub_key
+
+      def load
+        cofig_hash = JSON.parse(File.read("#{TEST_SUITE_BASE_PATH}/signing-config.json"))
+        @request_time = cofig_hash["request_time"]
+        @app_uuid = cofig_hash["app_uuid"]
+        @mauth_client = MAuth::Client.new(
+          app_uuid: @app_uuid,
+          private_key_file: File.join(TEST_SUITE_BASE_PATH, cofig_hash["private_key_file"])
+        )
+        @pub_key = File.read("#{TEST_SUITE_BASE_PATH}/signing-params/rsa-key-pub")
+      end
+
+      def cases
+        Dir.children("#{CASE_PATH}")
+      end
+    end
+  end
+
+  class CaseParser
+    def initialize(case_name)
+      @case_name = case_name
+    end
+
+    def req_attrs
+      @req_attrs ||= begin
+        JSON.parse(File.read(file_by_ext('req'))).tap do |attrs|
+          if attrs.has_key?('body_filepath')
+            attrs['body'] = File.read("#{CASE_PATH}/#{case_name}/#{attrs['body_filepath']}")
+          end
+        end
+      end
+    end
+
+    def sts
+      File.read(file_by_ext('sts'))
+    end
+
+    def sig
+      File.read(file_by_ext('sig'))
+    end
+
+    def auth_headers
+      JSON.parse(File.read(file_by_ext('authz')))
+    end
+
+    private
+
+    attr_reader :case_name
+
+    def file_by_ext(ext)
+      Dir.glob("#{CASE_PATH}/#{case_name}/*#{ext}").first
+    end
+  end
+end
